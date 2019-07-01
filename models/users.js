7e3e106e0b2467d5d8b8b3505626649a9a83d7a6
Rachel Tezza <racheletezza@gmail.com>
2019-07-01 15:29:06 -0400

    add new playlist button path fixed

--- models/users.js
+++ models/users.js
@@ -30,7 +30,6 @@ const UserSchema = new mongoose.Schema({
  name: {type: String, required: true},
  username: {type: String, required: true},
  password: {type: String, required: true},
- userId: mongoose.Types.ObjectId
 })
 
 /* Step 3
