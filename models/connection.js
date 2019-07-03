2bdcc9412fa519496e9ff71753f901edf512f61e
Rachel Tezza <racheletezza@gmail.com>
2019-07-03 14:16:14 -0400

    changed db connection

--- models/connection.js
+++ models/connection.js
@@ -18,9 +18,24 @@ const connectionString = process.env.MONGODB_URI || "mongodb://localhost/users";
  *
  * NOTE: newUrlParser diables a deprecation warning
  */
-mongoose.connect(connectionString, { useNewUrlParser: true})
-  .then(() => {
-    console.log("connected to mongo at: " + connectionString);
+// mongoose.connect(connectionString, { useNewUrlParser: true})
+//   .then(() => {
+//     console.log("connected to mongo at: " + connectionString);
+//   });
+
+  if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+  }
+  else {
+    mongoose.connect('mongodb://localhost/<db-name>');
+  }
+  mongoose.connection.on('error', function(err) {
+    console.error('MongoDB connection error: ' + err);
+    process.exit(-1);
+    }
+  );
+  mongoose.connection.once('open', function() {
+    console.log("Mongoose has connected to MongoDB!");
   });
 
 
